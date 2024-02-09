4351c883f91b2501586eb37f414b053f5dcfd27d
animesh-77 <74370416+animesh-77@users.noreply.github.com>
2024-02-09 17:04:12 +0000

    Time: 36 ms (57.52%), Space: 16.5 MB (59.40%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.py
+++ 2235-add-two-integers/2235-add-two-integers.py
@@ -1,4 +1,20 @@
 class Solution:
     def sum(self, num1: int, num2: int) -> int:
-        return num1+num2
+        # return num1+num2 # non optimal solution
         
+        mask= 0b111111111 # 511
+
+        a, b= num1& mask, num2&mask
+        
+
+        while b>0:
+            
+            carry= a&b
+            a = (a^b) & mask
+            
+            b= (carry << 1) & mask
+        
+        if a>>8 & 1 == 1:
+            # result is negative
+            return -self.sum(~a, 1)
+        return a
