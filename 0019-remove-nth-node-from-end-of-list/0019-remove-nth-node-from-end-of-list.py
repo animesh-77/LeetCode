30156725399af6f99c3d2eefa9a1205f0a0f1dc8
animesh-77 <74370416+animesh-77@users.noreply.github.com>
2024-02-20 20:44:00 +0000

    Time: 27 ms (97.83%), Space: 16.5 MB (95.91%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -6,33 +6,30 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         
-        node_list= [head]
-    
-        while head.next is not None:
-            node_list.append(head.next)
-            head= head.next
-            
+        total= 0
+        head2= head
+        while head2 is not None:
+            total +=1
+            head2= head2.next
+        
+        # print(total)
         # node_list[1].next= node_list[3]
         # return node_list[0]
     
-    
-    
-        if n == len(node_list):
-            if len(node_list)>=2:
-                return node_list[1]
-            else:
-                return None
+        if total-n == 0:
+            return head.next
         
-        if n==1:
-            if len(node_list)==1:
-                return None
-            else:
-                node_list[-n-1].next= None
-                return node_list[0]
+        node= head
+        for i in range(0, total-n):
+            if i== total-n-1:
+                try:
+                    node.next= node.next.next
+                except:
+                    node.next= None
             
-        node_list[-n-1].next= node_list[-n+1]
+            else:
+                node= node.next
         
-        return node_list[0]
         
-            
+        return head
         
