7c7a8a1914d4f63e96f446b5f4f2e31be6036e3a
animesh-77 <74370416+animesh-77@users.noreply.github.com>
2024-11-22 18:20:47 +0000

    Time: 120 ms (20.37%), Space: 19.5 MB (32.16%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
@@ -1,6 +1,13 @@
 class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
         
+        from collections import Counter
+
+    
+
+        
+
+
         # assume first col not flipped
         m_row, n_col= len(matrix), len(matrix[0])
         
@@ -17,12 +24,17 @@ class Solution:
                 else:
                     list_flip[row] += "1"
         
-        all_ops= {}
-        max_rows= 0
-        for op in list_flip:
-            all_ops[op] = all_ops.get(op, 0)+ 1
-            max_rows = all_ops[op] if (max_rows < all_ops[op]) else max_rows
-            
+        # all_ops= {}
+        # max_rows= 0
+        # for op in list_flip:
+        #     all_ops[op] = all_ops.get(op, 0)+ 1
+        #     max_rows = all_ops[op] if (max_rows < all_ops[op]) else max_rows
+        # Count the occurrences of each string
+        counter = Counter(list_flip)
+
+        # Find the string with the maximum count
+        most_frequent = max(counter, key=counter.get)
+        max_rows = counter[most_frequent]
         return max_rows
                     
                     
